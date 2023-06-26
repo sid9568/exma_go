c53da76a681b817717573c562a80a2fdaa136fe1
sid9568 <sid20319@gmail.com>
2023-06-26 18:09:03 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :exam do
     get 'home/index'
   end
+   namespace :exam do
+    get 'pg/index'
+    get 'ug/index'
+    get 'home/index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
